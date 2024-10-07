5673bbbb512c8d532ea51eea5eed7214173235bd
Bapa <bapa.bc04@gmail.com>
2024-10-07 16:00:10 -0300

    Created a '<div>' element in product-list.component.html to understand '*ngFor' directive and then commented the code. Deleted the code from product-list.component.ts and created 'listOfString' property to loop through it in product-list.component.html. Deleted all the '<a>' elements in main-menu.component.html, leaving only one to loop through it with '*ngFor' using a property created in main-menu.component.ts called 'mainMenuItems'.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,36 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
-  // name = "Ñañomaru";
-
-  addToCart: number = 0;
-
-  product = {
-    name: "iPhone X",
-    price: 789,
-    color: "Black Titanium",
-    discount: 8.5,
-    inStock: 10,
-    pImage: '/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice() {
-    return (this.product.price - (this.product.price * this.product.discount / 100)).toFixed(2);
-  }
-
-  onNameChange(event: any) {
-    // this.name = event.target.value;
-  }
-
-  decrementCartValue() {
-    if(this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue() {
-    if(this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 }
