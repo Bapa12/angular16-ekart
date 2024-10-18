192895afef690c637d8b2b7616509ac1b84fa147
Bapa <bapa.bc04@gmail.com>
2024-10-18 12:21:31 -0300

    Modified setbackground.directive.ts to use 'Renderer2'

--- src/app/customDirectives/setbackground.directive.ts
+++ src/app/customDirectives/setbackground.directive.ts
@@ -1,17 +1,23 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[setBackground]'
 })
 export class SetBackground implements OnInit{
   // private element: ElementRef;
+  // private renderer: Renderer2;
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
     // this.element = element;
+    // this.renderer = renderer;
   }
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#28282B';
-    this.element.nativeElement.style.color = 'white';
+    // this.element.nativeElement.style.backgroundColor = '#28282B';
+    // this.element.nativeElement.style.color = 'white';
+
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#28282B');
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    // this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example title');
   }
 }
